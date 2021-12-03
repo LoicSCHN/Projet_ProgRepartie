6175656d9a9ec7428b553cd076bd500b2fc1e04f
LoicSCHN <loic.cham74@gmail.com>
2021-12-03 11:15:19 +0100

    Semaphore

--- PrototypeClientServeur/CS.cpp
+++ PrototypeClientServeur/CS.cpp
@@ -251,6 +251,65 @@ void* fonctionThreadEmetteur (void * params){
 
 int main(int argc, char * argv[]){
 
+
+  // ----------------------------SEMAPHORE-------------------------------------
+
+  
+  
+  int clesem = ftok(argv[3], atoi(argv[4]));
+
+  int nbSem = atoi(argv[1]);
+
+  int idSem=semget(clesem, nbSem, IPC_CREAT | IPC_EXCL | 0600);
+  
+  if(idSem == -1){
+    perror("erreur semget : ");
+    exit(-1);
+  }
+
+  printf("sem id : %d \n", idSem);
+
+
+  
+  // initialisation des sémaphores a la valeur passée en parametre (faire autrement pour des valeurs différentes ):
+ 
+  ushort tabinit[nbSem];
+  for (int i = 0; i < nbSem; i++) tabinit[i] = atoi(argv[2]);;
+ 
+
+  union semun{
+    int val;
+    struct semid_ds * buf;
+    ushort * array;
+  } valinit;
+  
+  valinit.array = tabinit;
+
+  if (semctl(idSem, nbSem, SETALL, valinit) == -1){
+    perror("erreur initialisation sem : ");
+    exit(1);
+  }
+
+  /* test affichage valeurs des sémaphores du tableau */
+  valinit.array = (ushort*)malloc(nbSem * sizeof(ushort)); // pour montrer qu'on récupère bien un nouveau tableau dans la suite
+
+  if (semctl(idSem, nbSem, GETALL, valinit) == -1){
+    perror("erreur initialisation sem : ");
+    exit(1);
+  } 
+   
+  printf("Valeurs des sempahores apres initialisation [ "); 
+  for(int i=0; i < nbSem-1; i++){
+    printf("%d, ", valinit.array[i]);
+  }
+  printf("%d ] \n", valinit.array[nbSem-1]);
+
+  free(valinit.array);
+// ----------------------------Fin SEMAPHORE-------------------------------------
+
+
+
+
   if (argc < 4){
     printf("utilisation: %s  numero_port ip_serveur port_serveur \n", argv[0]);
     return 1;
